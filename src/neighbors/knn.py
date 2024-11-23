b0b47fba47278d6f73734abea3134dc22a65a448
Thanh Vu <thanhauco@gmail.com>
2024-11-23 10:45:00 -0800

    Feat: Implement K-Nearest Neighbors

---
+++ src/neighbors/knn.py
@@ -0,0 +1,20 @@
+import numpy as np
+from collections import Counter
+
+class KNeighborsClassifier:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        return [self._predict(x) for x in X]
+
+    def _predict(self, x):
+        distances = [np.sqrt(np.sum((x_train - x)**2)) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
