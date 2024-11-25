b37ccbef0fb5feb16e5a18da13fe74fcfadbcd8f
Thanh Vu <thanhauco@gmail.com>
2024-11-25 11:50:00 -0800

    Feat: Implement StandardScaler and MinMaxScaler

---
+++ src/preprocessing/scaler.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class StandardScaler:
+    def __init__(self):
+        self.mean_ = None
+        self.scale_ = None
+
+    def fit(self, X):
+        self.mean_ = np.mean(X, axis=0)
+        self.scale_ = np.std(X, axis=0)
+        return self
+
+    def transform(self, X):
+        return (X - self.mean_) / self.scale_
